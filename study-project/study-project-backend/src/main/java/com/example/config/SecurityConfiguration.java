8ad896b49ee235421c450472eeeedcbc0610f123
fansg666 <2680315178@qq.com>
2023-11-18 21:05:24 +0800

    update4

--- study-project/study-project-backend/src/main/java/com/example/config/SecurityConfiguration.java
+++ study-project/study-project-backend/src/main/java/com/example/config/SecurityConfiguration.java
@@ -16,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
@@ -41,14 +44,29 @@ public class SecurityConfiguration {
                 .and()
                 .logout()
                 .logoutUrl("/api/auth/logout")
+                .logoutSuccessHandler(this::onAuthenticationSuccess)
                 .and()
                 .csrf()
                 .disable()
+                .cors()
+                .configurationSource(this.corsConfigurationSource())
+                .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(this::onAuthenticationFailure)
                 .and()
                 .build();
 
+    }
+    private CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.addAllowedOriginPattern("*");
+        cors.setAllowCredentials(true);
+        cors.addAllowedHeader("*");
+        cors.addAllowedMethod("*");
+        cors.addExposedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",cors);
+        return source;
     }
     @Bean
     public AuthenticationManager authenticationManager(HttpSecurity security) throws Exception{
@@ -65,7 +83,10 @@ public class SecurityConfiguration {
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         response.setCharacterEncoding("utf-8");
         //response.getWriter().write("成共");
-        response.getWriter().write(JSONObject.toJSONString(Restbean.success("登陆成工")));
+        if(request.getRequestURI().endsWith("/login"))
+          response.getWriter().write(JSONObject.toJSONString(Restbean.success("登陆成功")));
+        else if(request.getRequestURI().endsWith("/logout"))
+            response.getWriter().write(JSONObject.toJSONString(Restbean.success("退出登陆成功")));
     }
 
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
---
+++ study-project/study-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+import image from "@/assets/2be267d0f703918f501da513143d26975beec465.jpg";
+</script>
+
+<template>
+  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+    <div style="flex: 1">
+      <el-image style="width: 100%;height: 100%" fit="cover"
+                :src= 'image'/>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 30px;font-weight: bold">欢迎来到我们的学习平台</div>
+      <div style="margin-top: 10px">在这里你将可以与java之父王俊逸直接对话</div>
+      <div style="margin-top: 5px">在这里你将可以同性交友，因为都是学java的男的，没有女生ovo</div>
+    </div>
+    <div style="width: 500px;background-color: white">
+      <router-view/>
+   </div>
+  </div>
+</template>
+
+<style scoped>
+.welcome-title{
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+</style>
